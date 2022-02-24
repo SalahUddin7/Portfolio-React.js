06e2084304d50227f9c766e111cc7d36157a12f0
Salah Uddin <ziko.salahuddin@gmail.com>
2022-02-24 23:00:14 +0100

    About Section updated

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -9,28 +9,26 @@ export default function AboutSection() {
           <h1>About Me</h1>
         </div>
 
-        <div className="item2">
-          I am a Front end developer based in Stockholm, Sweden. Welcome to my
-          website, here you can find projects that I have developed. I like to
-          do modern web development and always curious about latest tech. Scroll
-          down to see some of my work!
-        </div>
+        <div className="flex-container item2">
+          <div className="about-paragraph">
+            <p>
+              I am a Front end developer based in Stockholm, Sweden. Welcome to
+              my website, here you can find projects that I have developed. I
+              like to do modern web development and always curious about latest
+              tech. Scroll down to see some of my work!{' '}
+            </p>
 
-        <div className="item3 profile-picture">
-          <img src={Profile} alt=" an immage of myself" />
-        </div>
-
-        {/* <div className="about about-flex">
-          <div>
-            I am a Front end developer based in Stockholm, Sweden. Welcome to my
-            website, here you can find projects that I have developed. I like to
-            do modern web development and always curious about latest tech.
-            Scroll down to see some of my work!
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste
+              sint in, odit odio suscipit sed facilis perferendis deleniti
+              assumenda, saepe mollitia dolores.
+            </p>
           </div>
+
           <div className="profile-picture">
             <img src={Profile} alt=" an immage of myself" />
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
