202ac08f13ce0344a9fcc50f0a9a4e1d0af65c44
Salah Uddin <ziko.salahuddin@gmail.com>
2022-02-24 21:48:32 +0100

    CSS Grid added

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -4,12 +4,23 @@ import Profile from '../assets/salahuddin-profile-image-png.png';
 export default function AboutSection() {
   return (
     <div className="section">
-      <div className="about-grid">
-        <div>
+      <div className="grid-container">
+        <div className="item1">
           <h1>About Me</h1>
         </div>
 
-        <div className="about about-flex">
+        <div className="item2">
+          I am a Front end developer based in Stockholm, Sweden. Welcome to my
+          website, here you can find projects that I have developed. I like to
+          do modern web development and always curious about latest tech. Scroll
+          down to see some of my work!
+        </div>
+
+        <div className="item3 profile-picture">
+          <img src={Profile} alt=" an immage of myself" />
+        </div>
+
+        {/* <div className="about about-flex">
           <div>
             I am a Front end developer based in Stockholm, Sweden. Welcome to my
             website, here you can find projects that I have developed. I like to
@@ -19,7 +30,7 @@ export default function AboutSection() {
           <div className="profile-picture">
             <img src={Profile} alt=" an immage of myself" />
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
