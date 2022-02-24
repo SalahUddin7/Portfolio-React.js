ecc45d23d4e4df78caa4b2b005d63e93bc6f3873
Salah Uddin <ziko.salahuddin@gmail.com>
2022-02-24 18:52:44 +0100

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>This is SALAH </h1>
+        <NavBar />
       </header>
     </div>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Logo from '../assets/main-logo-salah.png';
+
+export default function NavBar() {
+  return (
+    <nav>
+      <div className="menu">
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#hero" className="main-logo">
+              <img
+                src={Logo}
+                alt="Logo made by Salah Uddin to represent this Portfolio website"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#tech">Tech</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
