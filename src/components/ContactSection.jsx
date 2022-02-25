3e8a9cb048796a0b8c23a392013c111eb033297d
Salah Uddin <ziko.salahuddin@gmail.com>
2022-02-25 14:16:52 +0100

    contact grid edited

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -6,36 +6,35 @@ import { FaRegEnvelope } from 'react-icons/fa';
 export default function ContactSection() {
   return (
     <div className="container-section">
-      <div className="contact-container a">
-        <div className="item11">
-          <h1>Contact Me-7</h1>
-        </div>
-
-        <div className="b">
-          <div>
+      <div className="contact-container">
+        <div class="contact-grid-container">
+          <div class="item1">
+            <h1>Contact Me</h1>
+          </div>
+          <div class="item2">
             <p>
-              1 Here is my contact information.I look forward to collaborate
-              with a great oganization.
+              Here is my contact information.I look forward to collaboratation.
             </p>
           </div>
-
-          <div className="">
-            2
-            <div>
-              <FaRegEnvelope />
-              <a href="mailto:ziko.salahuddin@gmail.com">
-                ziko.salahuddin@gmail.com
-              </a>
-            </div>
-            <div>
-              <FaPhoneAlt />
-              <a href="mailto:zzziko@gmail.com">0046 (0)73 642 2997</a>
-            </div>
-            <div>
-              <FaMapMarker />
-              <a href="https://goo.gl/maps/rHGJmtoi5sttNc5p8">
-                Stockholm, Sweden
-              </a>
+          <div class="item3">
+            {' '}
+            <div className="">
+              <div>
+                <FaRegEnvelope />
+                <a href="mailto:ziko.salahuddin@gmail.com">
+                  ziko.salahuddin@gmail.com
+                </a>
+              </div>
+              <div>
+                <FaPhoneAlt />
+                <a href="mailto:zzziko@gmail.com">0046 (0)73 642 2997</a>
+              </div>
+              <div>
+                <FaMapMarker />
+                <a href="https://goo.gl/maps/rHGJmtoi5sttNc5p8">
+                  Stockholm, Sweden
+                </a>
+              </div>
             </div>
           </div>
         </div>
