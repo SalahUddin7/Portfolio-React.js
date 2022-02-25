1c4eea33d37e1c63b498e487257e2e1a481aa7c2
Salah Uddin <ziko.salahuddin@gmail.com>
2022-02-25 14:41:23 +0100

    contact css edited

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -7,34 +7,32 @@ export default function ContactSection() {
   return (
     <div className="container-section">
       <div className="contact-container">
-        <div class="contact-grid-container">
-          <div class="item1">
-            <h1>Contact Me</h1>
+        <div className="item1">
+          <h1>Contact Me-10</h1>
+        </div>
+        <div className="item2">
+          <p>
+            Here is my contact information.I look forward to collaboratation.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            maxime nesciunt soluta fugit illo necessitatibus.
+          </p>
+        </div>
+        <div className="item3">
+          <div>
+            <FaRegEnvelope />
+            <a href="mailto:ziko.salahuddin@gmail.com">
+              ziko.salahuddin@gmail.com
+            </a>
           </div>
-          <div class="item2">
-            <p>
-              Here is my contact information.I look forward to collaboratation.
-            </p>
+          <div>
+            <FaPhoneAlt />
+            <a href="mailto:zzziko@gmail.com">0046 (0)73 642 2997</a>
           </div>
-          <div class="item3">
-            <div className="">
-              <div>
-                <FaRegEnvelope />
-                <a href="mailto:ziko.salahuddin@gmail.com">
-                  ziko.salahuddin@gmail.com
-                </a>
-              </div>
-              <div>
-                <FaPhoneAlt />
-                <a href="mailto:zzziko@gmail.com">0046 (0)73 642 2997</a>
-              </div>
-              <div>
-                <FaMapMarker />
-                <a href="https://goo.gl/maps/rHGJmtoi5sttNc5p8">
-                  Stockholm, Sweden
-                </a>
-              </div>
-            </div>
+          <div>
+            <FaMapMarker />
+            <a href="https://goo.gl/maps/rHGJmtoi5sttNc5p8">
+              Stockholm, Sweden
+            </a>
           </div>
         </div>
       </div>
