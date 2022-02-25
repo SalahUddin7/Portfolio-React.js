4d0bf8dd13891d92bad85d7868cf73535bf406f3
Salah Uddin <ziko.salahuddin@gmail.com>
2022-02-25 15:20:15 +0100

    contact section edited

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -7,14 +7,17 @@ export default function ContactSection() {
   return (
     <div className="container-section">
       <div className="contact-container">
-        <h1>Contact Me</h1>
-
-        <p>
-          Here is my contact information.I look forward to collaborate with a
-          great oganization.
-        </p>
-
-        <div className="flex-container item2">
+        <div className="item1">
+          <h1>Contact Me-10</h1>
+        </div>
+        <div className="item2">
+          <p>
+            Here is my contact information.I look forward to collaboratation.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            maxime nesciunt soluta fugit illo necessitatibus.
+          </p>
+        </div>
+        <div className="item3">
           <div>
             <FaRegEnvelope />
             <a href="mailto:ziko.salahuddin@gmail.com">
@@ -23,9 +26,8 @@ export default function ContactSection() {
           </div>
           <div>
             <FaPhoneAlt />
-            <a href="mailto:zzziko@gmail.com">0736422997</a>
+            <a href="mailto:zzziko@gmail.com">0046 (0)73 642 2997</a>
           </div>
-
           <div>
             <FaMapMarker />
             <a href="https://goo.gl/maps/rHGJmtoi5sttNc5p8">
